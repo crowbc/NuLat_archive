8031b324257e7f314ab240262012119a52c19e96
mjb22 <crowbc@hawaii.edu>
2022-08-03 13:19:30 -0400

    Final commit before starting build4

---
+++ build4/CMakeFiles/NuLat.dir/DependInfo.cmake
@@ -0,0 +1,66 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jack/Documents/geant4/NuLat/NuLat.cc" "/home/jack/Documents/geant4/NuLat/build4/CMakeFiles/NuLat.dir/NuLat.cc.o"
+  "/home/jack/Documents/geant4/NuLat/src/NuLatActionInitialization.cc" "/home/jack/Documents/geant4/NuLat/build4/CMakeFiles/NuLat.dir/src/NuLatActionInitialization.cc.o"
+  "/home/jack/Documents/geant4/NuLat/src/NuLatDetectorConstruction.cc" "/home/jack/Documents/geant4/NuLat/build4/CMakeFiles/NuLat.dir/src/NuLatDetectorConstruction.cc.o"
+  "/home/jack/Documents/geant4/NuLat/src/NuLatEventAction.cc" "/home/jack/Documents/geant4/NuLat/build4/CMakeFiles/NuLat.dir/src/NuLatEventAction.cc.o"
+  "/home/jack/Documents/geant4/NuLat/src/NuLatLightGuideParameterisation.cc" "/home/jack/Documents/geant4/NuLat/build4/CMakeFiles/NuLat.dir/src/NuLatLightGuideParameterisation.cc.o"
+  "/home/jack/Documents/geant4/NuLat/src/NuLatPhotoCathodeHit.cc" "/home/jack/Documents/geant4/NuLat/build4/CMakeFiles/NuLat.dir/src/NuLatPhotoCathodeHit.cc.o"
+  "/home/jack/Documents/geant4/NuLat/src/NuLatPhotoCathodeSensitiveDetector.cc" "/home/jack/Documents/geant4/NuLat/build4/CMakeFiles/NuLat.dir/src/NuLatPhotoCathodeSensitiveDetector.cc.o"
+  "/home/jack/Documents/geant4/NuLat/src/NuLatPrimaryGeneratorMessenger.cc" "/home/jack/Documents/geant4/NuLat/build4/CMakeFiles/NuLat.dir/src/NuLatPrimaryGeneratorMessenger.cc.o"
+  "/home/jack/Documents/geant4/NuLat/src/NuLatPrimaryParticleGenerator.cc" "/home/jack/Documents/geant4/NuLat/build4/CMakeFiles/NuLat.dir/src/NuLatPrimaryParticleGenerator.cc.o"
+  "/home/jack/Documents/geant4/NuLat/src/NuLatRun.cc" "/home/jack/Documents/geant4/NuLat/build4/CMakeFiles/NuLat.dir/src/NuLatRun.cc.o"
+  "/home/jack/Documents/geant4/NuLat/src/NuLatRunAction.cc" "/home/jack/Documents/geant4/NuLat/build4/CMakeFiles/NuLat.dir/src/NuLatRunAction.cc.o"
+  "/home/jack/Documents/geant4/NuLat/src/NuLatVoxelHit.cc" "/home/jack/Documents/geant4/NuLat/build4/CMakeFiles/NuLat.dir/src/NuLatVoxelHit.cc.o"
+  "/home/jack/Documents/geant4/NuLat/src/NuLatVoxelParameterisation.cc" "/home/jack/Documents/geant4/NuLat/build4/CMakeFiles/NuLat.dir/src/NuLatVoxelParameterisation.cc.o"
+  "/home/jack/Documents/geant4/NuLat/src/NuLatVoxelSensitiveDetector.cc" "/home/jack/Documents/geant4/NuLat/build4/CMakeFiles/NuLat.dir/src/NuLatVoxelSensitiveDetector.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "G4LIB_BUILD_DLL"
+  "G4UI_BUILD_XM_SESSION"
+  "G4UI_USE_QT"
+  "G4UI_USE_TCSH"
+  "G4UI_USE_XM"
+  "G4VIS_USE_OPENGL"
+  "G4VIS_USE_OPENGLQT"
+  "G4VIS_USE_OPENGLX"
+  "G4VIS_USE_OPENGLXM"
+  "G4VIS_USE_TOOLSSG"
+  "G4VIS_USE_TOOLSSG_QT_GLES"
+  "G4VIS_USE_TOOLSSG_X11_GLES"
+  "G4VIS_USE_TOOLSSG_XT_GLES"
+  "PTL_BUILD_DLL"
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NO_DEBUG"
+  "QT_OPENGL_LIB"
+  "QT_PRINTSUPPORT_LIB"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "/opt/applications/geant4/geant4-v11.0.2-installMT/include/Geant4"
+  "/usr/include/x86_64-linux-gnu/qt5"
+  "/usr/include/x86_64-linux-gnu/qt5/QtGui"
+  "/usr/include/x86_64-linux-gnu/qt5/QtCore"
+  "/usr/lib/x86_64-linux-gnu/qt5/mkspecs/linux-g++"
+  "/usr/include/x86_64-linux-gnu/qt5/QtOpenGL"
+  "/usr/include/x86_64-linux-gnu/qt5/QtWidgets"
+  "/usr/include/x86_64-linux-gnu/qt5/QtPrintSupport"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
